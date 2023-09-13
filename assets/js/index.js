f90ca127cda8d5b9b2e9314649eb55db1e908952
Mohamed Adel <134806641+mo-adel99@users.noreply.github.com>
2023-09-13 03:23:42 +0300

    h

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+function playVideo () {
+  const playBtn = document.querySelector("#thumbnail-button");
+
+  playBtn.addEventListener('click', () => {
+    playBtn.setAttribute("visible", false);
+    const testVideo = document.createElement( "video" );
+    const canplayWebm = testVideo.canPlayType( 'video/webm; codecs="vp8, vorbis"' );
+    if (canplayWebm == "") {
+      document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
+      document.querySelector("#paintandquest-video-mp4").play();
+    } else {
+      document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-webm");
+      document.querySelector("#paintandquest-video-webm").play();
+    }
+  });
+}
+
+AFRAME.registerComponent('mytarget', {
+  init: function () {
+    this.el.addEventListener('targetFound', event => {
+      console.log("target found");
+          playVideo() ;
+        
+      
+    });
+    this.el.addEventListener('targetLost', event => {
+      console.log("target found");
+    });
+  }
+});
