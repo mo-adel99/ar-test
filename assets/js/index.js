0d810c4746eaac330274f46c81b6d3f481cfcde6
Mohamed Adel <134806641+mo-adel99@users.noreply.github.com>
2023-09-13 03:50:42 +0300

    g

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,29 +3,13 @@ function playVideo () {
 
   playBtn.addEventListener('click', () => {
     playBtn.setAttribute("visible", false);
-    // const testVideo = document.createElement( "video" );
-    // const canplayWebm = testVideo.canPlayType( 'video/webm; codecs="vp8, vorbis"' );
-    // if (canplayWebm == "") {
-    //   document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
-    //   document.querySelector("#paintandquest-video-mp4").play();
-    // } else {
-    //   document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-webm");
-    //   document.querySelector("#paintandquest-video-webm").play();
-    // }
-
-    //   document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
-    //   document.querySelector("#paintandquest-video-mp4").play();
+    document.querySelector("#video").setAttribute("src", "#main-video");
     document.querySelector("#main-video").play();
   });
 }
 
 AFRAME.registerComponent('mytarget', {
   init: function () {
-    this.el.addEventListener('targetFound', event => {
       playVideo();
-    });
-    this.el.addEventListener('targetLost', event => {
-      console.log("target found");
-    });
   }
 });
