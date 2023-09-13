ccc3d7f269688d6e87c363631bc7ea6743946cd4
Mohamed Adel <134806641+mo-adel99@users.noreply.github.com>
2023-09-13 17:36:00 +0300

    s

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
 
-
-
+document.addEventListener("DOMContentLoaded", function(){
+  AFRAME.registerComponent('click-handler', {
+    init: function () {
+      
+      this.el.addEventListener('click', e => {
+        this.el.setAttribute("visible", false);
+        document.querySelector("#main-video").play();
+      });
+    }
+  });
+});
+  
