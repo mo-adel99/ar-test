69ef99a5b3a5fb287551cca076daff88cdee927d
Mohamed Adel <134806641+mo-adel99@users.noreply.github.com>
2023-09-13 17:04:28 +0300

    s

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,3 @@
 
 
 
-AFRAME.registerComponent('cursor-listener', {
-  init: function () {
-      this.el.addEventListener('click', function (evt) {
-      console.log('I was clicked at: ', evt.detail.intersection.point);
-    });
-  }
-});
