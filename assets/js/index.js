a855ec5aa2c96b91af0a30c35fe96fbea71d0db4
Mohamed Adel <134806641+mo-adel99@users.noreply.github.com>
2023-09-13 16:47:28 +0300

    s

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,7 @@
-// AFRAME.registerComponent('mytarget', {
-//   init: function () {
-    
-//     this.el.addEventListener('targetFound', event => {
-//       const playBtn = document.querySelector("#thumbnail-button");
-
-//       playBtn.addEventListener('click', () => {
-//         playBtn.setAttribute("visible", false);
-//         document.querySelector("#main-video").play();
-//       });
-    
-//     });
-//   }
-// });
-
-
 AFRAME.registerComponent('clickHandler', {
   init: function () {
     
-    this.el.addEventListener('click', event => {
-      // const playBtn = document.querySelector("#thumbnail-button");
+    this.el.addEventListener('click', e => {
       
       this.el.setAttribute("visible", false);
       document.querySelector("#main-video").play();
