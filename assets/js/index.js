3bdb465d4c5b0c77bef1aabfd70072f570cf8b66
Mohamed Adel <134806641+mo-adel99@users.noreply.github.com>
2023-09-14 20:47:31 +0300

    g

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,15 @@ document.addEventListener("DOMContentLoaded", function(){
   });
 });
   
+AFRAME.registerComponent('cc', {
+  init: function () {
+      const marker = document.querySelector("a-scene");
+
+      marker.addEventListener("targetFound", evt => {
+        setTimeout(evt => {
+          this.el.setAttribute("position", marker.getAttribute("position"));
+          this.el.setAttribute("rotation", marker.getAttribute("rotation"));
+        }, 500)
+      })
+  }
+});
